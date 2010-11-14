c34b9af907e287c41c586d29d3bc2f59f24c69e6
meichao <meichao@d7022953-1ca3-a944-a70e-6a56d19ef69b>
2010-11-14 08:47:20 +0000

    git-svn-id: http://59.39.61.138:8088/svn/junjie@539 d7022953-1ca3-a944-a70e-6a56d19ef69b

--- junjie/patches/scripts/理算系数/理算系数表建库脚本.sql
+++ junjie/patches/scripts/理算系数/理算系数表建库脚本.sql
@@ -25,3 +25,55 @@ create or replace table JJ_BD_ADJUSTMENTCOEFFICIENT  (
    constraint PK_JJ_BD_ADJUSTMENTCOEFFICIENT primary key (pk_adjustmentcoefficient),
    constraint UN_JJ_BD_ADJUSTMENTCOEFFICIENT unique (pk_invbasdoc)
 )
+
+
+--ÐÂ°æ
+-- Create table
+create table JJ_BD_ADJUSTMENTCOEFFICIENT
+(
+  PK_ADJUSTMENTCOEFFICIENT CHAR(20) not null,
+  PK_INVBASDOC             CHAR(20),
+  VINVSPEC                 VARCHAR2(50),
+  NADJUSTMENTCOEFFICIENT   NUMBER(20,8),
+  VMEMO                    VARCHAR2(200),
+  TS                       CHAR(19) default to_char(sysdate,'yyyy-mm-dd hh24:mi:ss'),
+  DR                       NUMBER(10),
+  VDEF1                    VARCHAR2(20),
+  VDEF2                    VARCHAR2(20),
+  VDEF3                    VARCHAR2(20),
+  VDEF4                    VARCHAR2(20),
+  VDEF5                    VARCHAR2(20),
+  VDEF6                    VARCHAR2(20),
+  VDEF7                    VARCHAR2(20),
+  VDEF8                    VARCHAR2(20),
+  VDEF9                    VARCHAR2(20),
+  VDEF10                   VARCHAR2(20)
+)
+tablespace NNC_DATA01
+  pctfree 10
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 256
+    next 256
+    minextents 1
+    maxextents unlimited
+    pctincrease 0
+  );
+-- Create/Recreate primary, unique and foreign key constraints 
+alter table JJ_BD_ADJUSTMENTCOEFFICIENT
+  add constraint PK_JJ_BD_ADJUSTMENTCOEFFICIENT primary key (PK_ADJUSTMENTCOEFFICIENT)
+  using index 
+  tablespace NNC_DATA01
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 256K
+    next 256K
+    minextents 1
+    maxextents unlimited
+    pctincrease 0
+  );
