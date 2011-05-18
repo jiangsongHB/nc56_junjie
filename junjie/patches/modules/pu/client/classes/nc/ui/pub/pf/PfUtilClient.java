5be66d33d75a92538a00a09ba8555e4ea07645d8
fushichao <fushichao@d7022953-1ca3-a944-a70e-6a56d19ef69b>
2011-05-18 08:06:00 +0000

    0000174: 收付通用流程中增加采购发票，参照过滤
    git-svn-id: http://59.39.61.138:8088/svn/junjie@807 d7022953-1ca3-a944-a70e-6a56d19ef69b

--- junjie/patches/modules/pu/client/classes/nc/ui/pub/pf/PfUtilClient.java
+++ junjie/patches/modules/pu/client/classes/nc/ui/pub/pf/PfUtilClient.java
@@ -398,8 +398,12 @@ public class PfUtilClient {
 			whereString = qcDLG.getWhereSQL();
 		} else
 			whereString = pkField + "='" + sourceBillId + "'";
-          //add by QuSida (佛山骏杰)2010-9-24
-		  // 采购发票参照采购应付单的过滤条件(是否红冲：否    是否暂估应付：是)
+		 //add by ouyangzhb 2011-05-06 
+		  // 采购发票参照采购应付单的过滤条件(是否红冲：否    是否暂估应付：是,是否生效）
+		if(currentBillType.trim().equals("25")&&billType.trim().equals("D1")){
+		whereString = whereString.substring(0, whereString.length()-1);
+		whereString = whereString+" and zb.isreded = 'N' and zb.zgyf = 1 and zb.dr = 0 and zb.sxbz ='10' )";
+		}
 
 		// fgj2001-12-06在条件语句的后边增加0(与公司无关)或1(与公司有关)
 		if (isQueryRelationCorp) {
