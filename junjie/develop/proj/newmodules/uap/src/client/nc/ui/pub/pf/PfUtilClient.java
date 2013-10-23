beac61b9be26a00cd093811d3de92c2d4fee85f8
ouyangzb <ouyangzb@d7022953-1ca3-a944-a70e-6a56d19ef69b>
2013-10-23 09:29:34 +0000

    已经开完票的单据不能被参照出来
    git-svn-id: http://59.39.61.138:8088/svn/junjie@1047 d7022953-1ca3-a944-a70e-6a56d19ef69b

--- junjie/develop/proj/newmodules/uap/src/client/nc/ui/pub/pf/PfUtilClient.java
+++ junjie/develop/proj/newmodules/uap/src/client/nc/ui/pub/pf/PfUtilClient.java
@@ -410,7 +410,7 @@ public class PfUtilClient {
 		if(currentBillType.trim().equals("25")&&billType.trim().equals("D1")){
 		whereString = whereString.substring(0, whereString.length()-1);
 		//add by zhang xiao wei and isnull(zb.zyx20,'N')='N' 处理参照回写的问题。
-		whereString = whereString+" and zb.isreded = 'N' and zb.zgyf = 1 and zb.dr = 0 and nvl(fb.ntotalinvoicenumber,0)< nvl(fb.dfshl,0))";
+		whereString = whereString+" and zb.isreded = 'N' and zb.zgyf = 1 and zb.dr = 0 and nvl(fb.ntotalinvoicenumber,0)< nvl(fb.dfshl,0)) and  fb.isverifyfinished = 'N' ";
 		}
 		
 		
