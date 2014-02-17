9a89a64ab16aaf3eda84a725f59b7e01eeb5de03
wanglei <wanglei@d7022953-1ca3-a944-a70e-6a56d19ef69b>
2014-02-17 10:16:22 +0000

    修复发票审批策略处理以及发票审核按钮相关业务。
    git-svn-id: http://59.39.61.138:8088/svn/junjie@1206 d7022953-1ca3-a944-a70e-6a56d19ef69b

--- junjie/develop/proj/newmodules/scmpub/src/public/nc/vo/scmpub/ITaxInvoiceApproveType.java
+++ junjie/develop/proj/newmodules/scmpub/src/public/nc/vo/scmpub/ITaxInvoiceApproveType.java
@@ -8,8 +8,8 @@ package nc.vo.scmpub;
  *
  */
 public interface ITaxInvoiceApproveType {
-	public static final int DEAL_BEFORE_AUDIT = 1;
+	public static final int DEAL_BEFORE_AUDIT = 2;
 	//审核前核销
-	public static final int DEAL_AFTER_AUDIT = 2;
+	public static final int DEAL_AFTER_AUDIT = 1;
 	//审核后核销
 }
