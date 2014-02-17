86e382e2fb4a0fde08624da1c5bf6663ece199e7
wanglei <wanglei@d7022953-1ca3-a944-a70e-6a56d19ef69b>
2014-02-17 02:57:40 +0000

    发票核销界面，发票查询条件增加非赠品固定条件。
    git-svn-id: http://59.39.61.138:8088/svn/junjie@1196 d7022953-1ca3-a944-a70e-6a56d19ef69b

--- junjie/patches/modules/so/client/classes/nc/ui/so/taxinvoice/VerifyPanel.java
+++ junjie/patches/modules/so/client/classes/nc/ui/so/taxinvoice/VerifyPanel.java
@@ -248,7 +248,7 @@ BillEditListener, BillEditListener2, MouseListener {
 			
 			//发票表体条件，是否核销完成
 			strWhere += " and isnull(so_saleinvoice_b.dr,0) = 0 and (isnull(so_saleinvoice_b.ntotaldealnum,0) < so_saleinvoice_b.nnumber  " +   // 发票行条件
-					"or isnull(so_saleinvoice_b.ntotaldealmny,0) < so_saleinvoice_b.nsummny ) ";
+					"or isnull(so_saleinvoice_b.ntotaldealmny,0) < so_saleinvoice_b.nsummny ) and isnull(blargessflag,'N') = 'N' ";  //2014-01-17 增加非赠品条件
 			
 			//费用发票条件
 			if (othersql == null || othersql.length() == 0) {  //允许覆盖的查询条件
